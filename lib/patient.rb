0c341c3ff9924ab8b1bdf2e8646b06c068c07da3
Oscar Shoudt <shoudto@gmail.com>
2019-05-11 15:15:24 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Patient 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_appointment(doctor, date)
+   # binding.pry 
+    Appointment.new(doctor,self, date)
+  end 
+  
+  def appointments
+    Appointment.all.select do |app|
+      app.patient == self 
+    end 
+  end 
+  
+end 
