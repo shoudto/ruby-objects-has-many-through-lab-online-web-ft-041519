16c7138a22d7f0bad9847068fb61cdb853856159
Oscar Shoudt <shoudto@gmail.com>
2019-05-11 15:43:31 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,8 +13,8 @@ class Doctor
     @@all 
   end
   
-  def new_appointment(date, patient)
-    Appointment.new(patient, self, date)
+  def new_appointment(date, name)
+    Appointment.new(doctor, self, date)
   end 
   
   def appointments
